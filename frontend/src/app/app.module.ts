152d9c6fd7009a1d780d6ab17784c1befec5f82a
alejandro-411 <castano.alejandro.3774@eam.edu.co>
2024-07-15 14:10:34 -0500

    made changes in backend and data_processor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DataTableComponent } from './data-table/data-table.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,14 @@ import { DataTableComponent } from './data-table/data-table.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/data-table/data-table.component.ts
+++ frontend/src/app/data-table/data-table.component.ts
@@ -6,14 +6,64 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.css']
 })
+
 export class DataTableComponent implements OnInit {
   data: any[] = [];
-  statistics: any[] = [];
+  statistics: any = {};
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-   this.http.get('')
   }
 
+
+/**
+ * Loads a CSV file and processes the data.
+ */
+ loadCSV(): void {
+    this.http.get('assets/resultados_personales.csv', { responseType: 'text' })
+      .subscribe(
+        data => this.processData(data),
+        error => console.error('Error loading CSV:', error)
+      );
+  } 
+
+  
+
+  /**
+   * Processes the data by making an HTTP GET request to 'http://localhost:5000/process'.
+   * It logs the response, loads the data, and loads the statistics.
+   * If an error occurs, it logs the error message.
+   */
+  processData(): void {
+    this.http.get('http://localhost:5000/process').subscribe(
+      response => {
+        console.log(response);
+        this.loadData();
+        this.loadStatistics();
+      },
+      error => console.error('Error processing data:', error)
+    );
+  }
+
+  /**
+   * Loads data from the server.
+   */
+  loadData(): void {
+    this.http.get<any[]>('http://localhost:5000/data').subscribe(
+      data => this.data = data,
+      error => console.error('Error loading data:', error)
+    );
+  }
+
+
+  /**
+   * Loads the statistics data from the server.
+   */
+  loadStatistics(): void {
+    this.http.get<any>('http://localhost:5000/statistics').subscribe(
+      stats => this.statistics = stats,
+      error => console.error('Error loading statistics:', error)
+    );
+  }
 }
