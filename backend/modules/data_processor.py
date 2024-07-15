130c34e1e66ebca64100dfed6f73718de66b6b8d
alejandro-411 <castano.alejandro.3774@eam.edu.co>
2024-07-15 10:42:30 -0500

    dataframes created along with its operations

--- backend/modules/data_processor.py
+++ backend/modules/data_processor.py
@@ -1,3 +1,29 @@
 import pandas as pd
-path = 'data/data1.xlsx'
-pd.read_excel(path)
+
+# Load data
+df_personal = pd.read_excel('backend/data/data1.xlsx')
+df_financial = pd.read_excel('backend/data/data2.xlsx')
+
+# Merge data
+df_merged = pd.merge(df_personal, df_financial, on='nit')
+
+df_merged['nombre completo'] = df_merged['nombre'] + ' ' + df_merged['apellido']
+df_merged['total'] = df_merged['ingresos'] - df_merged['egresos']
+
+
+stats = {
+    'suma de Ingresos': df_merged['ingresos'].sum(),
+    'suma de Egresos': df_merged['egresos'].sum(),
+    'promedio de Ingresos': df_merged['ingresos'].mean(),
+    'promedio de Egresos': df_merged['egresos'].mean(),
+    'ingreso Máximo': df_merged['ingresos'].max(),
+    'ingreso Mínimo': df_merged['ingresos'].min(),
+    'egreso Máximo': df_merged['egresos'].max(),
+    'egreso Mínimo': df_merged['egresos'].min()
+}
+
+# Save data
+
+df_stats = pd.DataFrame(list(stats.items()), columns=['descripción', 'valor'])
+df_merged.to_csv('backend/data/data_merged.csv', index=False)
+df_stats.to_csv('backend/data/data_stats.csv', index=False)
